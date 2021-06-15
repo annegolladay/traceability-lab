7e458eb945280b1cf028dfb3a1f4f5f068a03ac9
Anne Golladay <annegolladay@gmail.com>
2021-06-15 12:20:13 -0600

    saving

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
-const Rollbar = require('rollbar')
-let rollbar = new Rollbar({
-    accessToken:'',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-})
+// const Rollbar = require('rollbar')
+// let rollbar = new Rollbar({
+//     accessToken:'',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// })
 
 
 const app = express()
@@ -13,12 +13,12 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-    rollbar.info('html file served successfully')
+    // rollbar.info('html file served successfully')
 })
 
 
 const port = process.env.PORT || 4545
 
-app.use(rollbar.errorHandler())
+// app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`running on port: ${port}`))
